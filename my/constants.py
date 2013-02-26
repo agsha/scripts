39ad029160c1aebdbaa9808e56826535028d4bc7
Sharath Gururaj <e271828r@gmail.com>
2013-02-26 14:28:21 -0500

    added constants

---
+++ my/constants.py
@@ -0,0 +1,19 @@
+'''
+Created on Jun 17, 2012
+
+@author: sharath
+'''
+
+from os.path import abspath, expanduser
+
+DOWNLOAD_DIR = abspath(expanduser("~/Downloads"))
+MYSQL_64_URL = "http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.25-osx10.6-x86_64.tar.gz/from/http://mysql.mirrors.pair.com/"
+MYSQL_32_URL = "http://dev.mysql.com/get/Downloads/MySQL-5.5/mysql-5.5.25-osx10.5-x86.tar.gz/from/http://mysql.mirrors.pair.com/"
+USR_LOCAL = abspath("/usr/local")
+
+surveysidekick = "surveysidekick"
+researchbroker = "researchbroker"
+vialogues = "vialogues"
+nlt = "nlt"
+cas = "cas"
+user = "edlab"
